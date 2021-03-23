cd65bf28c45071d8a3291d75a5baec1a01492668
kMlad <kmlad98@gmail.com>
2021-03-23 05:51:07 +0100

    home page done fully

--- src/components/home-page/home-component.jsx
+++ src/components/home-page/home-component.jsx
@@ -1,25 +1,32 @@
 import React from "react";
-import Cactus from "../../images/cactus-pc.png";
+import CactusPC from "../../images/cactus-pc.png";
+import CactusMobile from "../../images/cactuses-mobile-mirror.png";
+import CactusMedium from "../../images/cactus-medium.png";
+
 import "./home-styles.scss";
 
 const HomePage = (props) => {
   return (
-    <div className="home-page">
-      <div className="introduction-container">
-        <h3 className="hello-heading">Hello I am Petar</h3>
-        <h1 className="job-title">
-          UX/UI & <br />
-          Graphic
-          <br />
-          Designer
-        </h1>
-        <p className="what-i-like">
-          I like to solve problems and create designs
-        </p>
-        <button className="home-btn">Let's talk!</button>
-      </div>
-      <div className="image-container">
-        <img src={Cactus} alt="kaktufce" className="kaktusce" />
+    <div className="home-page-outer">
+      <div className="home-page">
+        <div className="introduction-container">
+          <h3 className="hello-heading">Hello I am Petar</h3>
+          <h1 className="job-title">
+            UX/UI & <br />
+            Graphic
+            <br />
+            Designer
+          </h1>
+          <p className="what-i-like">
+            I like to solve problems and create designs
+          </p>
+          <button className="home-btn">Let's talk!</button>
+        </div>
+        <div className="image-container">
+          <img src={CactusPC} alt="kaktufce" className="kaktusce-pc" />
+          <img src={CactusMobile} alt="kaktufce" className="kaktusce-mobile" />
+          <img src={CactusMedium} alt="kaktufce" className="kaktusce-medium" />
+        </div>
       </div>
     </div>
   );
